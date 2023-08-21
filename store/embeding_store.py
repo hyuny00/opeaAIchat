68b3e0e5a562d21f065711e7447c453d0b543b82
hyun <10jqk1@kakao.com>
2023-08-21 21:41:02 +0900

    주석추가

--- store/embeding_store.py
+++ store/embeding_store.py
@@ -27,6 +27,11 @@ class EmbeddingStore:
         # self.embedding = HuggingFaceEmbeddings(model_name=self.model_name, model_kwargs=self.model_kwargs)
 
     def all_embedding_vector(self, storeName: str):
+
+        """
+        디렉토리의 모든 파일을 임베딩한다.
+
+        """
         pdf_documents = [
             os.path.join(self.file_path, filename)
             for filename in os.listdir(self.file_path)
@@ -52,7 +57,36 @@ class EmbeddingStore:
         print("Embed and create vector index")
         self.save_doc(storeName, split_docs)
 
+
+
+    def embedding_vector(self, fileName: str):
+
+        """
+        파일 한건을 임베딩한다.
+        """
+      
+        langchain_documents = []
+
+        file_extension = get_file_extension(fileName)
+        try:
+            loader = self._get_loader( os.path.join(self.file_path, fileName), file_extension)
+            data = loader.load()
+            langchain_documents.extend(data)
+
+        except Exception:
+
+            split_docs = self.text_splitter.split_documents(documents=langchain_documents)
+
+        print("Embed and create vector index")
+        self.save_doc(fileName, split_docs)
+
+
+
     def _get_loader(self, document, file_extension):
+
+        """
+        파일종류에따른 로드를 반환한다
+        """
         if file_extension == ".pdf":
             loader = PyPDFLoader(document)
 
@@ -70,11 +104,19 @@ class EmbeddingStore:
         return loader
 
     def save_doc(self, storeName, split_docs):
+
+        """
+        분할된 문서를 벡터저장소에 저장한다
+        """
         vectore_store = FAISS.from_documents(split_docs, embedding=self.embedding)
         vectore_store.save_local(self.store_path, index_name=storeName)
 
 
-def get_file_extension(file_name):
+def get_file_extension(file_name) -> str:
+
+    """
+    파일 확장자를 리턴한다
+    """
     file_extension = os.path.splitext(file_name)[1].lower()
 
     return file_extension
